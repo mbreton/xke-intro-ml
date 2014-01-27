a812f318fbbf1cd409ae9a145cb64de759451c55
mbreton <mbreton@xebia.fr>
2014-01-27 22:39:07 +0100

    Add automatic saving of the code into the localStorage

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,11 @@
-$(function(){
+$(function () {
     "use strict";
 
     // Dimmer initializing ...
     var $backdrop = $('#backdrop');
     var $iframe = $('#result');
+    var LOCALSTORAGE_KEY = 'com.xebia.xke.ml.code';
+    var code = '';
 
     // Ace editor initializing ...
     var editor = ace.edit("editor");
@@ -11,17 +13,27 @@ $(function(){
     editor.getSession().setMode("ace/mode/javascript");
     editor.setShowPrintMargin(false);
 
-    editor.on("change", _.debounce(function(e){
+    if (!!localStorage && !!localStorage.getItem(LOCALSTORAGE_KEY)) {
+        code = localStorage.getItem(LOCALSTORAGE_KEY);
+        editor.setValue(code);
+    }
+
+    editor.on("change", _.debounce(function () {
         $backdrop.addClass('active');
+        code = editor.getValue();
+        localStorage.setItem(LOCALSTORAGE_KEY, code);
         $iframe[0].contentWindow.location.reload();
     }, 500));
 
-    $iframe.load(_.bind(function(){
+    $iframe.load(_.bind(function () {
         $backdrop.removeClass('active');
-        var code = editor.getValue();
         var script = $iframe[0].contentWindow.document.createElement("script");
         script.type = "text/javascript";
         script.innerHTML = code;
-        $iframe[0].contentWindow.document.body.appendChild(script);
-    },this));
+        try{
+            $iframe[0].contentWindow.document.body.appendChild(script);
+        } catch (e){
+            console.warn('An error has been detected in the written code.', e);
+        }
+    }, this));
 });
